492b2f7c1df7ab4792fd1f025e6f8eb810395bf0
Sanil Shrestha <sanil@192-168-1-113.tpgi.com.au>
2024-01-27 23:16:38 +1100

    save state in local strorage

--- src/components/TodoItemComponent.vue
+++ src/components/TodoItemComponent.vue
@@ -12,7 +12,10 @@ defineProps({
 });
 
 defineEmits([
-    'toggle-complete'
+    'toggle-complete',
+    'toggle-editing',
+    'toggle-save',
+    'toggle-delete'
 ])
 </script>
 <template>
@@ -23,16 +26,20 @@ defineEmits([
                     <input type="checkbox" :id="'check-'+todoItem.id" :checked="todoItem.isCompleted" @input="$emit('toggle-complete', index)">
                 </span>
             </div>
-            <input v-if="todoItem.isEditing" type="text" class="form-control" :value="todoItem.todo" />
-            <span v-else class="form-control">{{ todoItem.todo }}</span>
+            <input v-if="todoItem.isEditing" type="text" class="form-control" :value="todoItem.todo" @input="$emit('toggle-save', index, $event.target.value)"/>
+            <span v-else class="form-control" :class="{'todo-completed': todoItem.isCompleted}">{{ todoItem.todo }}</span>
             <div class="input-group-append">
                 <span class="input-group-text">
-                    <Icon v-if="todoItem.isEditing" icon="ph:check-circle" class="icon check-icon" color="41b080" width="22" />
-                    <Icon v-else icon="ph:pencil-fill" class="icon edit-icon" color="41b080" width="22" />
-                    <Icon icon="ph:trash" class="icon trash-icon" color="f95e5e" width="22" />
+                    <Icon v-if="todoItem.isEditing" icon="ph:check-circle" class="icon check-icon" color="41b080" width="22"  @click="$emit('toggle-editing', index)"/>
+                    <Icon v-else icon="ph:pencil-fill" class="icon edit-icon" color="41b080" width="22" @click="$emit('toggle-editing', index)"/>
+                    <Icon icon="ph:trash" class="icon trash-icon" color="f95e5e" width="22" @click="$emit('toggle-delete', todoItem.id)"/>
                 </span>
             </div>
         </div>
     </li>
 </template>
-<style scoped></style>
+<style scoped>
+.todo-completed{
+    text-decoration: line-through;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,33 @@ import TodoCreateComponent from '@/components/TodoCreateComponent.vue';
 import TodoItemComponent from '@/components/TodoItemComponent.vue';
 import { Icon } from '@iconify/vue';
 import { uid } from 'uid';
-import { ref } from 'vue';
+import { ref, watch, computed } from 'vue';
 const todoList = ref([]);
 
-const createTodoListner = (todo) => {
+const saveInLocalStorage = () => {
+    localStorage.setItem("todoList", JSON.stringify(todoList.value));
+}
+
+const fetchFromLocalStorage = () => {
+    const savedTodoList = JSON.parse(localStorage.getItem("todoList"));
+    if (savedTodoList) {
+        todoList.value = savedTodoList;
+    }
+};
+
+fetchFromLocalStorage();
+
+watch(todoList, () => {
+    saveInLocalStorage();
+}, {
+    deep: true
+})
+
+const todosCompleted = computed(() => {
+    return todoList.value.every((todo) => todo.isCompleted);
+});
+
+const onCreateTodo = (todo) => {
     console.log(">>" + todo);
     todoList.value.push({
         id: uid(),
@@ -16,8 +39,22 @@ const createTodoListner = (todo) => {
     })
 }
 
-const toggleCompleteListner = (index) => {
-    console.log(index)
+const onToggleComplete = (index) => {
+    todoList.value[index].isCompleted = !todoList.value[index].isCompleted;
+}
+
+const onToggleEditing = (index) => {
+    todoList.value[index].isEditing = !todoList.value[index].isEditing;
+}
+
+const onToggleSave = (index, updatedTodo) => {
+    todoList.value[index].todo = updatedTodo;
+}
+
+const onToggleDelete = (todoId) => {
+    todoList.value = todoList.value.filter((todo) => {
+        return todo.id != todoId
+    })
 }
 
 </script>
@@ -25,10 +62,16 @@ const toggleCompleteListner = (index) => {
     <h1>Create todo</h1>
     <div class="col">
         <div class="col-6">
-            <TodoCreateComponent @create-todo-event="createTodoListner" />
+            <TodoCreateComponent @create-todo-event="onCreateTodo" />
             <hr />
+            <div v-if="todosCompleted && todoList.length > 0" class="alert alert-success mt-1 md-1">
+                <Icon icon="noto-v1:party-popper" />
+                <span class="pl-1">You have completed all your todos!</span>
+            </div>
             <ul class="list-group" v-if="todoList.length">
-                <TodoItemComponent v-for="(todoItem, index) in todoList" v-bind:key="todoItem.id" :todoItem="todoItem" :index="index" @toggle-complete="toggleCompleteListner"/>
+                <TodoItemComponent v-for="(todoItem, index) in todoList" v-bind:key="todoItem.id" :todoItem="todoItem"
+                    :index="index" @toggle-complete="onToggleComplete" @toggle-editing="onToggleEditing"
+                    @toggle-save="onToggleSave" @toggle-delete="onToggleDelete" />
             </ul>
 
             <div v-else class="alert alert-info">
